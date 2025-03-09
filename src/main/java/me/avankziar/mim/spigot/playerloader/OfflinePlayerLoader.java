ef1061e5a04ac4704753b0939ece9c5088e693f5
Avankziar <48923414+Avankziar@users.noreply.github.com>
2025-03-09 19:05:26 +0100

    Delete src/main/java/me/avankziar/mim directory

--- src/main/java/me/avankziar/mim/spigot/playerloader/OfflinePlayerLoader.java
+++
@@ -1,9 +0,0 @@
-package main.java.me.avankziar.mim.spigot.playerloader;
-
-import org.bukkit.OfflinePlayer;
-import org.bukkit.entity.Player;
-
-public interface OfflinePlayerLoader
-{
-	Player loadPlayer(OfflinePlayer offlinePlayer);
-}
